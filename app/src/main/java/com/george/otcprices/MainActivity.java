0a874d6e46323ef9a6fbe5f4fb9e7c0fcdbe797a
farmaker47 <farmaker47@yahoo.cm>
2018-05-05 11:03:39 +0300

    JobScheduler,notifications and FCM ready

--- app/src/main/java/com/george/otcprices/MainActivity.java
+++ app/src/main/java/com/george/otcprices/MainActivity.java
@@ -26,7 +26,6 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
-
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
